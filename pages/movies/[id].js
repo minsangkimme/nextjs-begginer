52128fb6ea4f159f50d41665dabde189466a4875
minsangkimme <minsang.tech@gmail.com>
2022-03-10 13:24:17 +0900

    next dynamic router

---
+++ pages/movies/[id].js
@@ -0,0 +1,9 @@
+import { useRouter } from 'next/router';
+
+export default function Detail() {
+    const router = useRouter();
+    console.log(router);
+    return (
+        <h4>{router.query.title || 'Loding...'}</h4>
+    );
+}
